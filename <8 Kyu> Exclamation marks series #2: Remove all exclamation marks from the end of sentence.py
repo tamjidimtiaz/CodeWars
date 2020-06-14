83f6b7c020e631c1b429f82ffa771b2dd3cb2437
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2020-06-14 22:02:37 +0600

    Create <8 Kyu> Exclamation marks series #2: Remove all exclamation marks from the end of sentence.py

---
+++ <8 Kyu> Exclamation marks series #2: Remove all exclamation marks from the end of sentence.py
@@ -0,0 +1,18 @@
+'''
+
+Description:
+
+Remove all exclamation marks from the end of sentence.
+Examples
+
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+
+'''
+
+def remove(s):
+   return remove(s[0:-1]) if s[-1] == "!" else s
