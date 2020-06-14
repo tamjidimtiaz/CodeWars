66919c62d0d2db8da268d1722b849e9803b92913
Tamjid Imtiaz <tamjidimtiaz1995@gmail.com>
2020-06-14 21:39:33 +0600

    Update Minimum amount of fuel needed to get some iron ingots.py

--- Minimum amount of fuel needed to get some iron ingots.py
+++ Minimum amount of fuel needed to get some iron ingots.py
@@ -1,8 +1,10 @@
-Just another day in the world of Minecraft, Steve is getting ready to start his next exciting project -- building a railway system!
+''' Just another day in the world of Minecraft, Steve is getting ready to start his next exciting project -- 
+building a railway system!
 
 But first, Steve needs to melt some iron ores in the furnace to get the main building blocks of rails -- iron ingots. Alt text
 
-Each iron ingot takes 11 seconds* to produce. Steve needs a lot of them, and he has the following fuel options to add into the furnace:
+Each iron ingot takes 11 seconds* to produce. Steve needs a lot of them, and he has the following fuel options 
+to add into the furnace:
 
     Buckets of lava, each lasts 800 seconds* Alt text
     Blaze rod, each lasts 120 seconds Alt text
@@ -10,8 +12,10 @@ Each iron ingot takes 11 seconds* to produce. Steve needs a lot of them, and he 
     Blocks of Wood, each lasts 15 seconds Alt text
     Sticks, each lasts 1 second* Alt text
 
-In Ruby: Write a function calc_fuel that calculates the minimum amount of fuel needed to produce a certain number of iron ingots. This function should return a hash of the form {:lava => 2, :blaze_rod => 1, :coal => 1, :wood => 0, :stick => 0} In JavaScript: Write a function calcFuel that calculates the minimum amount of fuel needed to produce a certain number of iron ingots. This function should return an object of the form {lava: 2, blazeRod: 1, coal: 1, wood: 0, stick: 0} In Python: Write a function calc_fuel that calculates the minimum amount of fuel needed to produce a certain number of iron ingots. This function should return a dictionary of the form {"lava": 2, "blaze rod": 1, "coal": 1, "wood": 0, "stick": 0}
-
+In Python: Write a function calc_fuel that calculates the minimum amount of fuel needed 
+to produce a certain number of iron ingots. This function should return a dictionary of the form 
+{"lava": 2, "blaze rod": 1, "coal": 1, "wood": 0, "stick": 0}
+'''
 def calc_fuel(n):
     total_time = n*11
     dict = {"lava": 0,
